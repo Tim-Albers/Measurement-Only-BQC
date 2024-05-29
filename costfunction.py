4cfcd64c96c2a1a06e618c5e03faf10c8f709ec5
Tim-Albers <alberstim12@gmail.com>
2024-05-29 15:22:25 +0200

    new num_runs

--- costfunction.py
+++ costfunction.py
@@ -177,8 +177,8 @@ if __name__ == "__main__":
     parser.add_argument('--emission_fidelity', type=float)
     args = parser.parse_args()
     parameter_values = [args.p_loss_init, args.coherence_time, args.single_qubit_depolar_prob, args.ms_depolar_prob, args.emission_fidelity]
-    num_runs = 1000 #20000  #18444 #73777 # Times the simulation is repeated - determines confidence interval of average outcome based on Hoeffding's bound
-    run_amount = 100 #10000 # The simulation is run in batches of run_amount to limit memory usage
+    num_runs = 100 #20000  #18444 #73777 # Times the simulation is repeated - determines confidence interval of average outcome based on Hoeffding's bound
+    run_amount = 10 #10000 # The simulation is run in batches of run_amount to limit memory usage
     script_path = '/home/timalbers/CODE/Measurement-Only-BQC/Simulationscript.py'
     baseline_path = '/home/timalbers/CODE/Measurement-Only-BQC/baseline.yaml'
     # Run the "simulation"

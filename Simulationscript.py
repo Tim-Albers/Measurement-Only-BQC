6ada4b03404bf6a5c04bff4777025fafbed5a197
Tim-Albers <alberstim12@gmail.com>
2024-06-11 11:01:41 +0200

    Per parameter search

--- Simulationscript.py
+++ Simulationscript.py
@@ -27,6 +27,8 @@ from instructions_local import XX
 from argparse import ArgumentParser
 import ast
 
+ns.util.simpools.MICROSECOND=1000.0 # factor to convert simulation time to nanoseconds
+
 steady_param_yaml = "/home/timalbers/CODE/Measurement-Only-BQC/steady_params.yaml" # Path to yaml file containing the paramters that are not varied over
 
 with open(steady_param_yaml) as f: #find parameters as stored in the yaml file
---
+++ per_parameter.py
@@ -0,0 +1,69 @@
+import sys
+import csv
+import numpy as np
+from argparse import ArgumentParser
+import yaml
+from yaml.loader import SafeLoader
+import subprocess
+import math
+
+steady_param_yaml = "/home/timalbers/CODE/Measurement-Only-BQC/steady_params.yaml" # Path to yaml file containing the paramters that are not varied over
+
+with open(steady_param_yaml) as f: #find parameters as stored in the yaml file
+    steady_params = yaml.load(f, Loader=SafeLoader)
+
+def find_error_prob(num_runs, run_amount, opt_params, script_path):
+    outcomes = []
+    runtimes = []
+    iterations = math.floor(num_runs/run_amount)
+    if iterations == 0:
+        raise IterationError()
+    last_bit = num_runs - iterations*run_amount
+    command = ['python', str(script_path), "--opt_params", str(opt_params), "--run_amount", str(run_amount)]
+    for k in range(iterations):
+        try:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if process.returncode == 0:
+                meas_outcome, runtime = stdout.decode().split(',')
+                outcomes.append(float(meas_outcome))
+                runtimes.append(float(runtime))
+            else:
+                error_mes = stderr.decode()
+                print(f"Error running simulation script:\n {error_mes}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running the script: {e}")
+        print(f"Iteration {k} of {iterations} complete")
+    if last_bit != 0:
+        command = ['python', str(script_path), "--opt_params", str(opt_params), "--run_amount", str(last_bit)]
+        try:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if process.returncode == 0:
+                meas_outcome, runtime = stdout.decode().split(',')
+                outcomes.append(float(meas_outcome))
+                runtimes.append(float(runtime))
+            else:
+                error_mes = stderr.decode()
+                print(f"Error running simulation script:\n {error_mes}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error running the script: {e}")
+    avg_outcome = sum(outcomes)/len(outcomes)
+    avg_runtime = sum(runtimes)/len(runtimes)
+    print("succesprob: ", avg_outcome)
+    if avg_outcome is not None:
+        return avg_outcome, avg_runtime
+    else:
+        print('No valid values found in for finding average outcome')
+
+
+
+p_loss_init_values = np.linspace(0.8846, 0.95, 10)
+
+if __name__ == '__main__':
+    script_path = '/home/timalbers/CODE/Measurement-Only-BQC/Simulationscript.py'
+    for p_loss in p_loss_init_values:
+        opt_params = steady_params.copy()
+        opt_params['p_loss_init'] = float(p_loss)
+        avg_outcome, avg_runtime = find_error_prob(70000, 10000, opt_params, script_path)
+        print(f"p_loss_init: {p_loss}, succesprob: {avg_outcome}, avg runtime: {avg_runtime} ms")

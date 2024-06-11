0700c4457c71bb8e803d7660a302478efaad8a47
Tim-Albers <alberstim12@gmail.com>
2024-06-11 11:13:34 +0200

    bugfix

--- per_parameter.py
+++ per_parameter.py
@@ -12,6 +12,15 @@ steady_param_yaml = "/home/timalbers/CODE/Measurement-Only-BQC/steady_params.yam
 with open(steady_param_yaml) as f: #find parameters as stored in the yaml file
     steady_params = yaml.load(f, Loader=SafeLoader)
 
+    # Default parameters that must be in opt_params
+param_base_dict = {
+    "p_loss_init": 0.8847,
+    "coherence_time": 62000000,
+    "single_qubit_depolar_prob": 0.02,
+    "ms_depolar_prob": 0.1,
+    "emission_fidelity": 0.947
+}
+
 def find_error_prob(num_runs, run_amount, opt_params, script_path):
     outcomes = []
     runtimes = []
@@ -56,14 +65,16 @@ def find_error_prob(num_runs, run_amount, opt_params, script_path):
     else:
         print('No valid values found in for finding average outcome')
 
-
-
 p_loss_init_values = np.linspace(0.8846, 0.95, 10)
 
 if __name__ == '__main__':
     script_path = '/home/timalbers/CODE/Measurement-Only-BQC/Simulationscript.py'
     for p_loss in p_loss_init_values:
         opt_params = steady_params.copy()
+        # Ensure all required parameters are present in opt_params
+        for key, value in param_base_dict.items():
+            if key not in opt_params:
+                opt_params[key] = value
         opt_params['p_loss_init'] = float(p_loss)
         avg_outcome, avg_runtime = find_error_prob(70000, 10000, opt_params, script_path)
         print(f"p_loss_init: {p_loss}, succesprob: {avg_outcome}, avg runtime: {avg_runtime} ms")

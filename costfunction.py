b90738812c41ed94b0d2430a2a9f78122a3585ab
Tim-Albers <alberstim12@gmail.com>
2024-06-13 17:12:49 +0200

    very few runs for a test round

--- costfunction.py
+++ costfunction.py
@@ -74,7 +74,7 @@ def delete_data(data_filename):
             datafile.write('')
     except Exception as e:
         print(f"An error occurred while deleting data: {e}")ssh q   
-        
+
 
 def find_error_prob(num_runs, run_amount, opt_params, script_path):
     outcomes = []
@@ -180,8 +180,8 @@ if __name__ == "__main__":
     parser.add_argument('--emission_fidelity', type=float)
     args = parser.parse_args()
     parameter_values = [args.p_loss_init, args.coherence_time, args.single_qubit_depolar_prob, args.ms_depolar_prob, args.emission_fidelity]
-    num_runs =  70000 #20000  #18444 #73777 # Times the simulation is repeated - determines confidence interval of average outcome based on Hoeffding's bound
-    run_amount =  10000 #10000 # The simulation is run in batches of run_amount to limit memory usage
+    num_runs =  10 #20000  #18444 #73777 # Times the simulation is repeated - determines confidence interval of average outcome based on Hoeffding's bound
+    run_amount =  10 #10000 # The simulation is run in batches of run_amount to limit memory usage
     script_path = '/home/timalbers/CODE/Measurement-Only-BQC/Simulationscript.py'
     baseline_path = '/home/timalbers/CODE/Measurement-Only-BQC/baseline.yaml'
     # Run the "simulation"
